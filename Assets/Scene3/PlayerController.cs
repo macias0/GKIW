45537e1c9dd2f3c4dfa161d130a490ef38c9b825
Maciej Gurgul <maciej.gurgul95@gmail.com>
2018-04-10 20:54:59 +0200

    Update

--- Assets/Scene3/PlayerController.cs
+++ Assets/Scene3/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 
 
 	private CharacterController controller;
+	private Vector3 moveDirection = Vector3.zero;
 	// Use this for initialization
 	void Start () {
 		controller = GetComponent<CharacterController> ();
@@ -13,13 +14,21 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-		Vector3 moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
-		moveDirection = transform.TransformDirection (moveDirection);
-		moveDirection *= speed;
-		controller.Move(moveDirection * Time.deltaTime);
+
+		if (controller.isGrounded) 
+		{
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("Jump"))
+				moveDirection.y = jumpSpeed;
+
+		}
+		moveDirection.y += Physics.gravity.y * Time.deltaTime;
+		controller.Move (moveDirection * Time.deltaTime);
 	}
 
 
-	public float speed = 1.0F;
+	public float speed = 10.0F;
+	public float jumpSpeed = 6.0F;
 }
---
+++ Assets/Scene3/PlayerController2.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour {
+
+
+	private Rigidbody rigidbody;
+//	private Vector3 moveDirection = Vector2.zero;
+
+	public float speed = 10.0F;
+	public float jumpSpeed = 10.0F;
+	public float rotationSpeed = 100.0F;
+
+	private float distToGround;
+
+	private bool isGrounded()
+	{
+//		Physics.Raycast (transform.position, Vector3.down, 10);
+		return Physics.Raycast(transform.position, Vector3.down, distToGround + 0.1f);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		Collider collider = GetComponent<Collider> ();
+		distToGround = collider.bounds.extents.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 euler = transform.localRotation.eulerAngles;
+		if (Input.GetKey (KeyCode.Z)) 
+		{
+			euler.x += rotationSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.X)) 
+		{
+			euler.x -= rotationSpeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey (KeyCode.Q)) 
+		{
+			euler.y -= rotationSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.E)) 
+		{
+			euler.y += rotationSpeed * Time.deltaTime;
+		}
+		if(transform.localRotation.eulerAngles != euler)
+			transform.localRotation = Quaternion.Euler (euler);
+	}
+
+
+	void FixedUpdate()
+	{
+		Vector3 moveDirection = Vector3.zero;
+		if (isGrounded()) 
+		{
+			moveDirection = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+			if (Input.GetButton ("Jump"))
+				moveDirection.y = jumpSpeed;
+//
+		}
+		//moveDirection.y += Physics.gravity.y * Time.deltaTime;
+		rigidbody.AddForce (moveDirection, ForceMode.Force);
+
+//		if (Input.GetKey (KeyCode.UpArrow)) 
+//		{
+//			rigidbody.AddForce(Vector3.forward * speed, ForceMode.Force);
+//		}
+//
+//		if (Input.GetKey (KeyCode.DownArrow)) 
+//		{
+//			rigidbody.AddForce(Vector3.back * speed, ForceMode.Force);
+//		}
+//
+//		if (Input.GetKey (KeyCode.LeftArrow)) 
+//		{
+//			rigidbody.AddForce(Vector3.left * speed, ForceMode.Force);
+//		}
+//
+//		if (Input.GetKey (KeyCode.RightArrow)) 
+//		{
+//			rigidbody.AddForce(Vector3.right * speed, ForceMode.Force);
+//		}
+
+	}
+}
