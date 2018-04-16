be2c847e65749c47be4891b343be5ddef0bdbed9
Maciej Gurgul <maciej.gurgul95@gmail.com>
2018-04-16 20:38:13 +0200

    Update

---
+++ Assets/Scene4/PathBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		if(draw)
+			for(int i=0; i<path.Count; i++)
+			{
+
+				Vector3 pos = path [i];
+				pos.y += 2.0F;
+				Debug.DrawLine (path [i], pos, Color.red);
+			}
+	}
+
+	public bool draw = false;
+	public List<Vector3> path = new List<Vector3>();
+	//public GameObject[] pathList;
+}
